00aacd6630ec10e07067f7e74f5aed629e2ae32d
alifudin-a <alifudinalfarizi@gmail.com>
2022-08-08 00:27:30 +0700

    env app port

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,6 @@ const app: Express = express();
 app.use(bodyParser.json())
 app.use(router);
 
-app.listen(5000, () => {
+app.listen(process.env.APP_PORT, () => {
   console.log("app started on port: 5000");
 });
