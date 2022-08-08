7dde6934fbcf7fc78c9e74530fc15b70a6276449
alifudin-a <alifudinalfarizi@gmail.com>
2022-08-08 09:46:47 +0700

    controller untuk auth awal

---
+++ src/controllers/auth.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response } from "express";
+import { login } from "../models/auth";
+import bcrypt from "bcrypt";
+
+const router = express.Router();
+
+router.post("/login", async (req: Request, res: Response) => {
+  let username: string = req.body.username;
+  let password: string = req.body.password;
+  
+  const data = await login(username);
+  if (!data) {
+    res.json({
+      code: 401,
+      message: "Login gagal!",
+    });
+  } else {
+    bcrypt
+      .compare(password, data.password)
+      .then((result: boolean) => {
+        if (result) {
+          res.json({
+            code: 200,
+            message: "Login berhasil!",
+            data: data,
+          });
+        } else {
+          res.json({
+            code: 401,
+            message: "Login gagal!",
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+});
+
+export default router;
