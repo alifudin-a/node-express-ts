a7417c277d1988a0df75e5ef9027c6be9d867899
alifudin-a <alifudinalfarizi@gmail.com>
2022-08-08 10:32:09 +0700

    jwt awal

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,16 +1,24 @@
 import express, { Request, Response } from "express";
 import { login } from "../models/auth";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.post("/login", async (req: Request, res: Response) => {
-  let username: string = req.body.username;
-  let password: string = req.body.password;
+  const username: string = req.body.username;
+  const password: string = req.body.password;
+
+  if (!(username || password) || !(username && password)) {
+    return res.status(400).json({
+      code: 400,
+      message: "Username atau Password tidak boleh kosong!",
+    });
+  }
 
   const data = await login(username);
   if (!data) {
-    res.json({
+    return res.status(401).json({
       code: 401,
       message: "Login gagal!",
     });
@@ -19,18 +27,27 @@ router.post("/login", async (req: Request, res: Response) => {
       .compare(password, data.password)
       .then((result: boolean) => {
         if (result) {
-          res.json({
+          // jwt
+          const token = jwt.sign(
+            {
+              username: data.username,
+              role: data.role,
+            },
+            process.env.JWT_TOKEN_KEY as string,
+            {
+              expiresIn: "2h",
+            }
+          );
+
+          return res.status(200).json({
             code: 200,
             message: "Login berhasil!",
             data: {
-              user: {
-                username: data.username,
-                role: data.role,
-              },
+              token: token,
             },
           });
         } else {
-          res.json({
+          return res.status(401).json({
             code: 401,
             message: "Login gagal!",
           });
