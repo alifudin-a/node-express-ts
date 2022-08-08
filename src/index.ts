bb0691ac742bd07e4937fa87d8b3101f1eb649a8
alifudin-a <alifudinalfarizi@gmail.com>
2022-08-08 10:57:42 +0700

    middleware jwt acces token untuk service todo

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 dotenv.config();
 import { todo, auth } from "./routes/routes";
+import verifyToken from "./middlewares/auth";
 
 const app: Express = express();
 
 app.use(bodyParser.json());
-app.use("/api/v1", todo);
+app.use("/api/v1/service", verifyToken, todo);
 app.use("/api/v1/auth", auth);
 
 app.listen(process.env.APP_PORT, () => {
